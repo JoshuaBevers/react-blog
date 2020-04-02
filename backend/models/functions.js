d3c6095d550282468c7d251a7eb4c0f33081d965
infernezor@gmail.com <infernezor@gmail.com>
2020-04-02 11:38:11 -0400

    Basic setup for mvc. Added lowlevel database. Set up folders for future use.

---
+++ backend/models/functions.js
@@ -0,0 +1,12 @@
+class Functions {
+  constructor(id, author, title, content) {
+    this.author = author;
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+
+  //functions
+}
+
+module.exports = Functions;
